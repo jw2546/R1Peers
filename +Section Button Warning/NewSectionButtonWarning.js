a567294caee963644cdcedb732ec8d9018792c0e
jw2546 <51129948+jw2546@users.noreply.github.com>
2023-06-15 15:28:06 -0400

    Add files via upload
    Custom code for displaying an alert message when users use the "+ Section" button from the "Sections" tab of the "Settings" page of a course.

---
+++ +Section Button Warning/NewSectionButtonWarning.js
@@ -0,0 +1,6 @@
+// Post an alert when +Section is clicked
+// Last Edited Aug 29, 2022 by jw2546
+
+$( "#add_section_form button" ).click(function() {
+    alert('Enrollment changes for manually created sections, such as adding or dropping students, will not be made through the Registrar. All changes must be made manually. Before adding other users, you will need to add yourself to the new section via the People page.');
+});
